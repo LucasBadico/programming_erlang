e5ee969168d502f3329c4d5c48f890ed582b9150
Lucas Badico <lgsantana1@stefanini.com>
2019-08-12 22:25:52 -0300

    middle of chapter 3

--- chapter_3/shop.erl
+++ chapter_3/shop.erl
@@ -1,9 +1,10 @@
 -module(shop).
--export([total/1]).
+-export([total/1, total_old/1, cost/1]).
+-import(my_lists, [map/2, sum/1]).
 
+total_old([{What,N} |T]) -> cost(What) * N + total(T);
+total_old([]) -> 0.
 
-total([{What,N} |T]) -> cost(What) * N + total(T);
-total([]) -> 0.
 
 cost(oranges) -> 5;
 cost(newspaper) -> 8;
@@ -11,3 +12,5 @@ cost(apples) -> 2;
 cost(pears) -> 9;
 cost(milk) -> 7.
 
+
+total(L) -> sum([shop: cost(Item) * Qtd || {Item, Qtd} <- L]).
